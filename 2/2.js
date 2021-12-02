2fd699eb4c528705745ccbbb58fb8382258a2faf
sandervspl <sandervispoel@hotmail.com>
2021-12-02 23:11:12 +0100

    Day 2:2

---
+++ 2/2.js
@@ -0,0 +1,19 @@
+/**
+ * https://adventofcode.com/2021/day/2#part2
+ * What do you get if you multiply your final horizontal position by your final depth?
+ */
+
+const data = require('../services/fileToString').fileToString('input').split('\n');
+let h = d = a = 0;
+
+for (const value of data) {
+  const [type, v] = value.split(' ');
+  if (type[0] === 'u') a -= +v;
+  else if (type[0] === 'd') a += +v;
+  else if (type[0] === 'f') {
+    h += +v;
+    d += (a * +v);
+  }
+}
+
+console.log({answer: h * d});
