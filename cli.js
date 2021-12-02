a260ad4deea6086045229211ca780da99a5f4b10
sandervspl <sandervispoel@hotmail.com>
2021-12-02 18:01:44 +0100

    Initial commit

---
+++ cli.js
@@ -0,0 +1,14 @@
+const { fork } = require('child_process');
+
+// Get day number from args
+const [,,...args] = process.argv
+const [day] = args
+
+// Let user know he is dumb
+if (day == null) {
+  console.error('ERROR: PASS A DAY NUMBER');
+  process.exit(1);
+}
+
+// Run day file with node
+fork(`${day}/index.js`);
