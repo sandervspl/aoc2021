e9fa28493dc419c5dd6d89385a1975f23aabc06b
sandervspl <sandervispoel@hotmail.com>
2021-12-02 19:20:19 +0100

    day 1:1

--- 1/index.js
+++ 1/index.js
@@ -1,1 +1,17 @@
-console.log(1);
+/**
+ * count the number of times a depth measurement increases from the previous measurement
+ */
+
+const { fileToString } = require('../services/fileToString');
+
+let inc = 0;
+
+fileToString("input")
+  .split('\n')
+  .map(Number)
+  .reduce((prev, cur) => {
+    if (prev < cur) inc++;
+    return cur;
+  });
+
+console.log({answer: inc});
