3c73756bbad17ad896f4b2e3fb3f625e04592d5b
sandervspl <sandervispoel@hotmail.com>
2021-12-06 18:54:27 +0100

    Add Eslint, editor config
    Update .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 13,
+  },
+  globals: {
+    inputFile: true,
+    module: true,
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': [0],
+    'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }],
+    'keyword-spacing': ['error', {
+      'before': true,
+      'after': true,
+      'overrides': {
+        'return': { 'after': true },
+        'throw': { 'after': true },
+        'case': { 'after': true }
+      }
+    }],
+    'no-multiple-empty-lines': ['error', { 'max': 2, 'maxEOF': 1 }],
+    'no-trailing-spaces': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'space-before-blocks': 'error',
+    'space-before-function-paren': ['error', {
+      'anonymous': 'always',
+      'named': 'never',
+      'asyncArrow': 'always'
+    }],
+    'space-in-parens': ['error', 'never'],
+    'space-infix-ops': 'error',
+    'eol-last': ['error', 'always'],
+    'no-inner-declarations': [0],
+  }
+};
